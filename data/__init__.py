debcd101616427d3cca0e090921cdd704934301b
Gurkirt Singh <guru094@gmail.com>
2019-03-29 14:59:37 -0400

    update init

--- data/__init__.py
+++ data/__init__.py
@@ -26,37 +26,6 @@ def base_transform_nimgs(images, size, mean, stds, seq_len=1):
     # return res_imgs
 
 
-# class BaseTransform:
-#     def __init__(self, size, means, stds):
-#         self.size = size
-#         self.means = means
-#         self.stds = stds
-
-#     def __call__(self, image, boxes=None, labels=None, seq_len=1):
-#         return base_transform_nimgs(image, self.size, self.means, self.stds, seq_len=seq_len), boxes, labels
-
-# def base_transform_nimgs(images, size, mean, stds, seq_len):
-#     res_imgs = []
-#     # print(images.shape)
-#     for i in range(seq_len):
-#         # img = Image.fromarray(images[i,:, :, :])
-#         # img = img.resize((size, size), Image.BILINEAR)
-#         img = cv2.resize(images[i, :, :, :], (size, size)).astype(np.float32)
-#         #img = images[i, :, :, :].astype(np.float32)
-#         # img  = np.asarray(img)
-#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) / 255.0
-#         res_imgs += [img]
-
-#     res_imgs = np.asarray(res_imgs)
-
-#     for i in range(seq_len):
-#         res_imgs[i, :, :, :] -= mean
-#         res_imgs[i, :, :, :] /= stds
-
-#     res_imgs = res_imgs.astype(np.float32)
-
-#     return res_imgs
-
 
 class BaseTransform:
     def __init__(self, size, means, stds):
