dba7616a99d8a9ce95f1525f7409a3bb3c9240f6
Gurkirt Singh <guru094@gmail.com>
2019-03-29 18:09:40 -0400

    readme update

--- prep/download_voc.sh
+++ prep/download_voc.sh
@@ -5,5 +5,6 @@ curl -LO http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.
 tar -xvf VOCtrainval_06-Nov-2007.tar
 tar -xvf VOCtest_06-Nov-2007.tar
 tar -xvf VOCtrainval_11-May-2012.tar
+
 mv VOCdevkit/ voc
 
